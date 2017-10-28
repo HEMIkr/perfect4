21f69d384b975c896627cee0c16240d7d6e0a43a
AleksanderWedrychowskiReady4s <aleksander.wedrychowski@ready4s.pl>
2017-10-28 21:24:31 +0200

    initial commit

---
+++ Sources/main.swift
@@ -0,0 +1,22 @@
+import PerfectLib
+import PerfectHTTP
+import PerfectHTTPServer
+
+let server = HTTPServer()
+
+var routes = Routes()
+routes.add(method: .get, uri: "/", handler: { request, response in
+    response.setHeader(.contentType, value: "text/html")
+    response.appendBody(string: "<html><title>Hello, world!</title><body>Hello, world!  I'm the brandly new Perfect, swiftly server ;)</body></html>")
+    response.completed()
+})
+
+server.addRoutes(routes)
+
+server.serverPort = 8181
+
+do {
+    try server.start()
+} catch PerfectError.networkError(let error, let message){
+    print("Network error thrown: \(error); \(message)")
+}
